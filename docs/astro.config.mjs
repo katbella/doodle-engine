8d71fb9f9228145ecaae01838a54b9ab8abf1159
Kat Bella <kat@katbella.com>
2026-02-15 21:09:47 -0800

    fix docs domain routing

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
 
 export default defineConfig({
+  base: '/doodle-engine/',
   integrations: [
     starlight({
       title: 'Doodle Engine',
